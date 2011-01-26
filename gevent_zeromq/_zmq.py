281791ed50ef2d28204ecf8d7b55ad392b2019ed
Travis Cline <travis.cline@gmail.com>
2011-01-25 18:01:28 -0600

    Switched to just wrapping send and recv.

--- gevent_zeromq/_zmq.py
+++ gevent_zeromq/_zmq.py
@@ -4,8 +4,9 @@ import zmq
 from zmq import *
 from zmq.core import context, socket
 
+from gevent import sleep
 from gevent.event import Event
-from gevent.hub import get_hub, _threadlocal
+from gevent.hub import get_hub
 
 
 class Context(context.Context):
@@ -40,6 +41,9 @@ class Socket(socket.Socket):
     The `__setup_events` method is triggered when the zmq.FD for the socket is
     marked as readable and triggers the necessary read and write events (which
     are waited for in the recv and send methods).
+
+    Some doubleunderscore prefixes are used to minimize pollution of
+    :class:`zmq.core.socket.Socket`'s namespace.
     """
 
     def __init__(self, context, socket_type):
@@ -47,8 +51,8 @@ class Socket(socket.Socket):
         self.__setup_events()
 
     def __setup_events(self):
-        self._read_ready = Event()
-        self._write_ready = Event()
+        self.__readable = Event()
+        self.__writable = Event()
         try:
             read_event = get_hub().reactor.read_event
             self._state_event = read_event(self.getsockopt(zmq.FD), persist=True)
@@ -63,56 +67,47 @@ class Socket(socket.Socket):
             return
         events = self.getsockopt(zmq.EVENTS)
         if events & zmq.POLLOUT:
-            self._write_ready.set()
+            self.__writable.set()
         if events & zmq.POLLIN:
-            self._read_ready.set()
+            self.__readable.set()
 
-    def _send_message(self, msg, flags=0):
-        flags |= zmq.NOBLOCK
-        while True:
+    def _wait_write(self):
+        self.__writable.clear()
+        self.__writable.wait()
+
+    def _wait_read(self):
+        self.__readable.clear()
+        self.__readable.wait()
+
+    def send(self, data, flags=0, copy=True, track=False):
+        while True: # Attempt to complete this operation indefinitely, blocking the current greenlet
+            # Marker as to if we've encountered EAGAIN yet. Required have zmq work well with deallocating many sockets
+            saw_eagain = False
             try:
-                super(Socket, self)._send_message(msg, flags)
-                return
+                # attempt the actual call, ensuring the zmq.NOBLOCK flag
+                return super(Socket, self).send(data, flags|zmq.NOBLOCK, copy, track)
             except zmq.ZMQError, e:
+                # if the raised ZMQError is not EAGAIN, reraise
                 if e.errno != zmq.EAGAIN:
                     raise
-                self._write_ready.clear()
-            self._write_ready.wait()
+                # if this is our first time seeing EAGAIN, avoid calling _wait_write just yet
+                if not saw_eagain:
+                    saw_eagain = True
+                    continue
+            # at this point we've seen two EAGAINs, defer to the event loop until we're notified the socket is writable
+            self._wait_write()
 
-    def _send_copy(self, msg, flags=0):
-        flags |= zmq.NOBLOCK
+    def recv(self, flags=0, copy=True, track=False):
         while True:
+            saw_eagain = False
             try:
-                super(Socket, self)._send_copy(msg, flags)
-                return
-            except zmq.ZMQError, e:
-                if e.errno != zmq.EAGAIN:
-                    raise
-                self._write_ready.clear()
-            self._write_ready.wait()
-
-    def _recv_message(self, flags=0, track=False):
-        flags |= zmq.NOBLOCK
-        while True:
-            try:
-                m = super(Socket, self)._recv_message(flags, track)
+                m = super(Socket, self).recv(flags|zmq.NOBLOCK, copy, track)
                 if m is not None:
                     return m
             except zmq.ZMQError, e:
                 if e.errno != zmq.EAGAIN:
                     raise
-                self._read_ready.clear()
-            self._read_ready.wait()
-
-    def _recv_copy(self, flags=0):
-        flags |= zmq.NOBLOCK
-        while True:
-            try:
-                m = super(Socket, self)._recv_copy(flags)
-                if m is not None:
-                    return m
-            except zmq.ZMQError, e:
-                if e.errno != zmq.EAGAIN:
-                    raise
-                self._read_ready.clear()
-            self._read_ready.wait()
+                if not saw_eagain:
+                    saw_eagain = True
+                    continue
+            self._wait_read()
