c96b918979435ecb1bf3b95a1fe4f03cec17f583
Travis Cline <travis.cline@gmail.com>
2011-01-30 16:35:20 -0600

    added type hinting to reduce overhead

--- gevent_zeromq/core.pyx
+++ gevent_zeromq/core.pyx
@@ -4,23 +4,24 @@ import zmq
 from zmq import *
 
 # imported with different names as to not have the star import try to to clobber (when building with cython)
-from zmq.core.context import Context as _original_Context
-from zmq.core.socket import Socket as _original_Socket
+from zmq.core.context cimport Context as _original_Context
+from zmq.core.socket cimport Socket as _original_Socket
 
 from gevent.event import Event
 from gevent.hub import get_hub
 
 # the number of EAGAINS to encounter before defering to file descriptor polling
-NUM_EAGAINS_BEFORE_DEFER = 5
+cdef int NUM_EAGAINS_BEFORE_DEFER = 5
 
+cdef class _Socket(_original_Socket)
 
-class _Context(_original_Context):
+cdef class _Context(_original_Context):
     """Replacement for :class:`zmq.core.context.Context`
 
     Ensures that the greened Socket below is used in calls to `socket`.
     """
 
-    def socket(self, socket_type):
+    def socket(self, int socket_type):
         """Overridden method to ensure that the green version of socket is used
 
         Behaves the same as :meth:`zmq.core.context.Context.socket`, but ensures
@@ -31,7 +32,7 @@ class _Context(_original_Context):
             raise ZMQError(ENOTSUP)
         return _Socket(self, socket_type)
 
-class _Socket(_original_Socket):
+cdef class _Socket(_original_Socket):
     """Green version of :class:`zmq.core.socket.Socket`
 
     The following four methods are overridden:
@@ -51,20 +52,21 @@ class _Socket(_original_Socket):
     Some doubleunderscore prefixes are used to minimize pollution of
     :class:`zmq.core.socket.Socket`'s namespace.
     """
+    cdef object __readable
+    cdef object __writable
+    cdef object _state_event
 
-    def __init__(self, context, socket_type):
+    def __init__(self, _Context context, int socket_type):
         super(Socket, self).__init__(context, socket_type)
         self.__setup_events()
 
     def close(self):
-        # close the _state_event event and delete it, keeps the number of
-        # of active file descriptors down
+        # close the _state_event event, keeps the number of active file descriptors down
         super(Socket, self).close()
         if hasattr(self, '_state_event'):
             self._state_event.cancel()
-            del(self._state_event)
 
-    def __setup_events(self):
+    cdef __setup_events(self) with gil:
         self.__readable = Event()
         self.__writable = Event()
         try:
@@ -89,21 +91,22 @@ class _Socket(_original_Socket):
         if events & zmq.POLLIN:
             self.__readable.set()
 
-    def _wait_write(self):
+    cdef _wait_write(self) with gil:
         self.__writable.clear()
         self.__writable.wait()
 
-    def _wait_read(self):
+    cdef _wait_read(self) with gil:
         self.__readable.clear()
         self.__readable.wait()
 
-    def send(self, data, flags=0, copy=True, track=False):
+    def send(self, object data, int flags=0, bint copy=True, bint track=False):
         # Marker as to if we've encountered EAGAIN yet. Required have zmq work well with deallocating many sockets
-        num_eagains = 0
+        cdef int num_eagains = 0
+        flags = flags | NOBLOCK
         while True: # Attempt to complete this operation indefinitely, blocking the current greenlet
             try:
                 # attempt the actual call, ensuring the zmq.NOBLOCK flag
-                return super(Socket, self).send(data, flags|zmq.NOBLOCK, copy, track)
+                return _original_Socket.send(self, data, flags, copy, track)
             except zmq.ZMQError, e:
                 # if the raised ZMQError is not EAGAIN, reraise
                 if e.errno != zmq.EAGAIN:
@@ -115,12 +118,13 @@ class _Socket(_original_Socket):
             # at this point we've seen enough EAGAINs, defer to the event loop until we're notified the socket is writable
             self._wait_write()
 
-    def recv(self, flags=0, copy=True, track=False):
-        num_eagains = 0
+    def recv(self, int flags=0, bint copy=True, bint track=False):
+        cdef int num_eagains = 0
+        flags = flags | NOBLOCK
         while True:
             try:
-                return super(Socket, self).recv(flags|zmq.NOBLOCK, copy, track)
-            except zmq.ZMQError, e:
+                return _original_Socket.recv(self, flags, copy, track)
+            except ZMQError, e:
                 if e.errno != zmq.EAGAIN:
                     raise
                 if num_eagains < NUM_EAGAINS_BEFORE_DEFER:
