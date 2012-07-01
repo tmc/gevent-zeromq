27cb3e35de07e7c46783a9841c7b1ec0eeb7a475
Travis Cline <travis.cline@gmail.com>
2012-06-30 21:48:52 -0500

    Increase BlockingDetector timeout.

--- gevent_zeromq/tests.py
+++ gevent_zeromq/tests.py
@@ -3,7 +3,7 @@ from gevent_zeromq import zmq
 
 try:
     from gevent_utils import BlockingDetector
-    gevent.spawn(BlockingDetector(15))
+    gevent.spawn(BlockingDetector(25))
 except ImportError:
     print 'If you encounter hangs consider installing gevent_utils'
 
